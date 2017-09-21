6ab38faa5e01ddfdb311cce3d5237a2de40855bb
Wei Kin Huang <weikin.huang04@gmail.com>
2017-09-21 19:53:08 -0400

    Don't fail container for failing to make directories

--- containers/danbooru/container/etc/cont-init.d/20-data-dir.sh
+++ containers/danbooru/container/etc/cont-init.d/20-data-dir.sh
@@ -4,10 +4,12 @@ set -euo pipefail
 IFS=$'\n\t'
 
 # create data directories
+mkdir -p /var/www/danbooru2/shared/public/data
+chown danbooru:danbooru /var/www/danbooru2/shared/public/data || true
+
 s6-setuidgid danbooru mkdir -p \
     /var/www/danbooru2/shared/public/data/preview \
-    /var/www/danbooru2/shared/public/data/sample
+    /var/www/danbooru2/shared/public/data/sample || true
 
-chown danbooru:danbooru /var/www/danbooru2/shared/public/data || true
 chown danbooru:danbooru /var/www/danbooru2/shared/public/data/preview || true
 chown danbooru:danbooru /var/www/danbooru2/shared/public/data/sample || true
